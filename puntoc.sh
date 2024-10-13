c502d2c44b636571c1e3d03d517d2d20e590a733
LucianoVerdecanna <luverdecanna1@gmail.com>
2024-10-13 19:05:40 +0000

    Finalizacion punto c

--- puntoc.sh
+++ puntoc.sh
@@ -10,4 +10,14 @@ id tester1
 sudo usermod -aG grupodevops devops1
 sudo usermod -aG grupodevelopers devops1
 id devops1
-sudo usermod -aG grupodevops devops2
+sudo usermod -aG grupodevops devops1
+sudo chmod -R 750 Examanes-UTN/alumnos1
+sudo chmod -R 760 Examanes-UTN/alumnos2
+sudo chmod -R 700 Examanes-UTN/alumnos3
+sudo chmod -R 775 Examanes-UTN/profesores
+whoami > validar.txt
+cat validar.txt
+whoami > Examanes-UTN/alumnos1/validar.txt
+whoami > Examanes-UTN/alumnos2/validar.txt
+whoami > Examanes-UTN/alumnos3/validar.txt
+whoami > Examanes-UTN/profesores/validar.txt
